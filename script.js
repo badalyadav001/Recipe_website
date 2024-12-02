2270a2d6afd8a4e430f1c55f4635ce6ac7dd7d6c
Badal yadav <badalyadavseg@gmail.com>
2024-12-02 12:17:58 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+// Hamburger menu toggle functionality
+document.getElementById('hamburger').addEventListener('click', function () {
+    document.querySelector('.nav-links').classList.toggle('active');
+});
